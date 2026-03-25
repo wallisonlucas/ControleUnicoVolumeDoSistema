4668859040a3cc8e8c8ee7022b6664682e7a7551
Wallison Lucas <wallisonllucas@gmail.com>
2026-03-25 16:36:43 -0300

    Atividade Prática Atualizada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ControleVolume a = ControleVolume.getInstance();
+        ControleVolume b = ControleVolume.getInstance();
+
+        System.out.println("Mesma instância? " + (a == b));
+
+        BotaoFisicoVolume botao = new BotaoFisicoVolume();
+        PlayerMusica player = new PlayerMusica();
+        Alarme alarme = new Alarme();
+
+        player.mostrarVolume();
+
+        botao.pressionarMais();
+        botao.pressionarMais();
+
+        player.mostrarVolume();
+
+        alarme.tocar();
+
+        ControleVolume.getInstance().mutar();
+        player.mostrarVolume();
+
+        ControleVolume.getInstance().setVolume(95);
+        botao.pressionarMais();
+        botao.pressionarMais();
+
+        player.mostrarVolume();
     }
 }
